3a4d72ac1acc59cfacc32f2f9fd00b35f8e8255e
kbagwe <kbagwe@uncc.edu>
2021-09-26 01:15:59 -0400

    New leetcode medium problem

--- RunningSumof1Darray.py
+++ RunningSumof1Darray.py
@@ -6,5 +6,7 @@ class Solution:
             ans = [0] * len(nums)
             ans[0] = nums[0]
             for i in range(1, len(nums)):
+                print(ans[i], ans[i-1], nums[i])
                 ans[i] = ans[i - 1] + nums[i]
             return ans
+
